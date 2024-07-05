6e79ece9d038108db30c463315bd3dee985a204f
Marquan <marquan.bowman@outlook.com>
2024-07-05 12:55:40 -0400

    display and image functions

--- script.js
+++ script.js
@@ -66,3 +66,29 @@ function displayWeather(data) {
         showImage();
     }
 }
+
+function displayHourlyForecast(hourlyData){
+    const hourlyForecastDiv = document.getElementById('houry-forecast');
+    const next24Hours = hourlyData.slice(0, 8);
+
+    next24Hours.forEach(item => {
+        const dateTime = new Date(item.dt * 1000);
+        const hour = dateTime.getHours();
+        const temperature = Math.round(item.main.temp - 273.15);
+        const iconCode = item.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn${iconCode}.png`;
+
+        const hourlyItemHtml = `
+            <div class="hourly-item">
+                <span>${hour}:00</span>
+                <img src="${iconUrl}" alt="Hourly Weather Icon">
+                <span>${temperature}&deg;C</span>
+            </div>`;
+        hourlyForecastDiv.innerHTML += hourlyItemHtml;
+    });
+}
+
+function showImage() {
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.style.display = 'block'; // Make the image visible once it's loaded
+}
