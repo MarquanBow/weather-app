160120a03524b2651588a54ad7ad2ea1165cfe4e
Marquan <marquan.bowman@outlook.com>
2024-07-05 12:47:44 -0400

    update displayWeather function

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getWeather() {
     }
 
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const forecastrUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
     fetch(currentWeatherUrl)
         .then(response => response.json())
@@ -22,7 +22,7 @@ function getWeather() {
             alert('Error fetching current weather data. Please try again.');
         });
 
-    fetch(forecastrUrlUrl)
+    fetch(forecastUrl)
         .then(response => response.json())
         .then(data => {
             displayHourlyForecast(data.list);
@@ -38,4 +38,31 @@ function displayWeather(data) {
     const weatherInfoDiv = document.getElementById('weather-info');
     const weatherIcon = document.getElementById('weather-icon');
     const hourlyForecastDiv = document.getElementById('hourly-forecast');
+
+    // Clear previous content
+    weatherInfoDiv.innerHTML = '';
+    hourlyForecastDiv.innerHTML = '';
+    tempDivInfo.innerHTML = '';
+
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn${iconCode}@4x.png`;
+
+        const temperatureHTML = `<p>${temperature}&deg;C</p>`;
+        const weatherHTML = `
+            <p>${cityName}</p>
+            <p>${description}</p>`;
+
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHTML;
+        weatherIcon.src = iconUrl;
+        weatherIcon.alt = description;
+
+        showImage();
+    }
 }
